2eaef10b68918c607ffe2d0c22424baffaea8d5d
ZDVokoun <grebe_hygrology@aleeas.com>
2024-12-10 21:58:29 +0100

    Done task 37-2-2 and 37-2-S from KSP

--- ksp/2/2.cpp
+++ ksp/2/2.cpp
@@ -23,82 +23,62 @@ signed main(int argc, char *argv[]) {
   vector<vll> prevgraph = graph;
 
   vector<vbool> visited(R, vbool(S));
-  queue<pint> bordering;
-  for (int i = 0; i < R; i++) {
-    bordering.push({i, 0});
-    bordering.push({i, S - 1});
+  visited[0][0] = true;
+  visited[0][S - 1] = true;
+  visited[R - 1][0] = true;
+  visited[R - 1][S - 1] = true;
+
+  priority_queue<pair<ll, pint>, vector<pair<ll, pint>>,
+                 greater<pair<ll, pint>>>
+      bordering;
+  for (int i = 1; i < R - 1; i++) {
+    bordering.push({graph[i][0], {i, 0}});
+    bordering.push({graph[i][S - 1], {i, S - 1}});
     visited[i][0] = true;
     visited[i][S - 1] = true;
   }
-  for (int i = 0; i < S; i++) {
-    bordering.push({0, i});
-    bordering.push({R - 1, i});
+  for (int i = 1; i < S - 1; i++) {
+    bordering.push({graph[0][i], {0, i}});
+    bordering.push({graph[R - 1][i], {R - 1, i}});
     visited[0][i] = true;
     visited[R - 1][i] = true;
   }
-  while (true) {
-    queue<pint> q(bordering);
-    bordering = {};
-    vector<pint> neigh = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+  ll res = 0;
+
+  while (!bordering.empty()) {
+    auto cr = bordering.top();
+    bordering.pop();
+    // if (visited[cr.second.first][cr.second.second])
+    //   continue;
     pint cur;
+    queue<pint> q;
+    q.push(cr.second);
+    ll height = cr.first;
+    vector<pint> neigh = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
     while (!q.empty()) {
       cur = q.front();
       q.pop();
-      // if (visited[cur.first][cur.second])
-      //   continue;
+      res += height - graph[cur.first][cur.second];
       visited[cur.first][cur.second] = true;
       for (auto [di, dj] : neigh) {
         ll newi = cur.first + di;
         ll newj = cur.second + dj;
-        if (newi < 0 || newi > R - 1 || newj < 0 || newj > R - 1)
+        if (newi < 0 || newi > R - 1 || newj < 0 || newj > S - 1)
           continue;
         if (visited[newi][newj])
           continue;
-        if (graph[newi][newj] >= graph[cur.first][cur.second])
+        if (graph[newi][newj] <= height) {
           q.push({newi, newj});
+          res += height - graph[newi][newj];
+          graph[newi][newj] = height;
+        } else {
+          bordering.push({graph[newi][newj], {newi, newj}});
+        }
+        visited[newi][newj] = true;
       }
     }
-    vector<vll> components(R, vll(S, -1));
-    vll values;
-    ll k = 0;
-    bool con = false;
-    for (int i = 0; i < R; i++)
-      for (int j = 0; j < S; j++) {
-        if (visited[i][j])
-          continue;
-        con = true;
-        q.push({i, j});
-        values.push_back(INT64_MAX);
-        while (!q.empty()) {
-          cur = q.front();
-          q.pop();
-          if (components[cur.first][cur.second] != -1)
-            continue;
-          components[cur.first][cur.second] = k;
-          for (auto [di, dj] : neigh) {
-            ll newi = cur.first + di;
-            ll newj = cur.second + dj;
-            if (visited[newi][newj]) {
-              values[k] = min(values[k], graph[newi][newj]);
-              bordering.push({newi, newj});
-            } else {
-              q.push({newi, newj});
-            }
-          }
-        }
-        k++;
-      }
-    if (!con)
-      break;
-    for (int i = 0; i < R; i++)
-      for (int j = 0; j < S; j++)
-        if (components[i][j] != -1)
-          graph[i][j] = max(values[components[i][j]], graph[i][j]);
   }
-  ll res = 0;
-  for (int i = 0; i < R; i++)
-    for (int j = 0; j < S; j++)
-      res += graph[i][j] - prevgraph[i][j];
+
   cout << res << endl;
 
   return 0;
---
+++ ksp/2/S.cpp
@@ -0,0 +1,282 @@
+#include <cassert>
+#include <cctype>
+#include <iostream>
+#include <optional>
+#include <string>
+#include <vector>
+
+enum TokenType {
+  // Operátory
+  TK_NOT,
+  TK_EQUAL,
+  TK_GREATER,
+  TK_LESS,
+  TK_MINUS,
+  TK_PLUS,
+  TK_SEMICOLON,
+  TK_SLASH,
+  TK_STAR,
+  TK_NOT_EQUAL,
+  TK_EQUAL_EQUAL,
+  TK_GREATER_EQUAL,
+  TK_LESS_EQUAL,
+  TK_LBRACE,
+  TK_LPAREN,
+  TK_RBRACE,
+  TK_RPAREN,
+
+  // Klíčová slova
+  TK_ELSE,
+  TK_FOR,
+  TK_IF,
+  TK_PRINT,
+  TK_VAR,
+  TK_WHILE,
+
+  // literály
+  TK_NAME,
+  TK_NUMBER,
+};
+
+struct Token {
+  TokenType type;
+
+  // pouze u TK_NAME a TK_NUMBER, jinak ""
+  std::string value;
+
+  int row;
+  int col;
+
+  Token(TokenType t, int r = 0, int c = 0, std::string v = "")
+      : type{t}, row{r}, col{c}, value{v} {}
+};
+
+std::string token_type_to_str(TokenType t) {
+  switch (t) {
+  case TK_NOT:
+    return "NOT";
+  case TK_EQUAL:
+    return "EQUAL";
+  case TK_GREATER:
+    return "GREATER";
+  case TK_LESS:
+    return "LESS";
+  case TK_MINUS:
+    return "MINUS";
+  case TK_PLUS:
+    return "PLUS";
+  case TK_SEMICOLON:
+    return "SEMICOLON";
+  case TK_SLASH:
+    return "SLASH";
+  case TK_STAR:
+    return "STAR";
+  case TK_NOT_EQUAL:
+    return "NOT_EQUAL";
+  case TK_EQUAL_EQUAL:
+    return "EQUAL_EQUAL";
+  case TK_GREATER_EQUAL:
+    return "GREATER_EQUAL";
+  case TK_LESS_EQUAL:
+    return "LESS_EQUAL";
+  case TK_LBRACE:
+    return "LBRACE";
+  case TK_LPAREN:
+    return "LPAREN";
+  case TK_RBRACE:
+    return "RBRACE";
+  case TK_RPAREN:
+    return "RPAREN";
+  case TK_ELSE:
+    return "ELSE";
+  case TK_FOR:
+    return "FOR";
+  case TK_IF:
+    return "IF";
+  case TK_PRINT:
+    return "PRINT";
+  case TK_VAR:
+    return "VAR";
+  case TK_WHILE:
+    return "WHILE";
+  case TK_NAME:
+    return "NAME";
+  case TK_NUMBER:
+    return "NUMBER";
+  }
+  return "<invalid token value>";
+}
+
+struct Scanner {
+  std::string source;
+  bool error_occured = false;
+
+  // Míří na znak, který bude v dalším kroku čten
+  int row = 1, col = 1;
+
+  Scanner(std::string s) : source{s} {}
+
+  void advance(size_t i = 1) {
+    if (peek() == '\n') {
+      row++;
+      col = 0;
+    }
+    col++;
+    source.erase(0, i);
+  }
+  bool is_at_end() { return source.empty(); }
+
+  char peek() { return source.empty() ? '\0' : source[0]; }
+
+  bool match(char c) {
+    if (peek() == c) {
+      advance();
+      return true;
+    }
+    return false;
+  }
+
+  void throw_error() {
+    std::cout << "Error occured at row " << row << ", column " << col
+              << std::endl;
+    error_occured = true;
+  }
+};
+
+std::optional<Token> match_operator_token(Scanner &sc) {
+  if (sc.match('('))
+    return Token(TK_LPAREN, sc.row, sc.col - 1);
+  if (sc.match(')'))
+    return Token(TK_RPAREN, sc.row, sc.col - 1);
+  if (sc.match('['))
+    return Token(TK_LBRACE, sc.row, sc.col - 1);
+  if (sc.match(']'))
+    return Token(TK_RBRACE, sc.row, sc.col - 1);
+  if (sc.match('<')) {
+    if (sc.match('=')) {
+      return Token(TK_LESS_EQUAL, sc.row, sc.col - 1);
+    } else {
+      return Token(TK_LESS, sc.row, sc.col - 1);
+    }
+  }
+  if (sc.match('>')) {
+    if (sc.match('='))
+      return Token(TK_GREATER_EQUAL, sc.row, sc.col - 1);
+    else
+      return Token(TK_GREATER, sc.row, sc.col - 1);
+  }
+  if (sc.match('!')) {
+    if (sc.match('='))
+      return Token(TK_NOT_EQUAL, sc.row, sc.col - 1);
+    else
+      return Token(TK_NOT, sc.row, sc.col - 1);
+  }
+  if (sc.match('=')) {
+    if (sc.match('='))
+      return Token(TK_EQUAL_EQUAL, sc.row, sc.col - 1);
+    else
+      return Token(TK_EQUAL, sc.row, sc.col - 1);
+  }
+  if (sc.match(';'))
+    return Token(TK_SEMICOLON, sc.row, sc.col - 1);
+  if (sc.match('*'))
+    return Token(TK_STAR, sc.row, sc.col - 1);
+  if (sc.match('-'))
+    return Token(TK_MINUS, sc.row, sc.col - 1);
+  if (sc.match('+'))
+    return Token(TK_PLUS, sc.row, sc.col - 1);
+  if (sc.match('/'))
+    return Token(TK_SLASH, sc.row, sc.col - 1);
+  return std::nullopt;
+}
+
+std::optional<Token> match_digit_token(Scanner &sc) {
+  std::string num;
+  while (std::isdigit(sc.peek())) {
+    num += sc.peek();
+    sc.advance();
+  }
+  if (num.size() > 0 && std::isalpha(sc.peek())) {
+    sc.throw_error();
+  }
+  if (num.size() == 0)
+    return std::nullopt;
+  return Token(TK_NUMBER, sc.row, sc.col - 1, num);
+}
+
+std::optional<Token> match_keyword_or_name_token(Scanner &sc) {
+  std::string str;
+  while (std::isalpha(sc.peek())) {
+    str += sc.peek();
+    sc.advance();
+  }
+  if (str.size() == 0)
+    return std::nullopt;
+  if (str == "else")
+    return Token(TK_ELSE, sc.row, sc.col - 1);
+  if (str == "for")
+    return Token(TK_FOR, sc.row, sc.col - 1);
+  if (str == "if")
+    return Token(TK_IF, sc.row, sc.col - 1);
+  if (str == "print")
+    return Token(TK_PRINT, sc.row, sc.col - 1);
+  if (str == "var")
+    return Token(TK_VAR, sc.row, sc.col - 1);
+  if (str == "while")
+    return Token(TK_WHILE, sc.row, sc.col - 1);
+  return Token(TK_NAME, sc.row, sc.col - 1, str);
+}
+
+std::optional<Token> lex_one(Scanner &sc) {
+  // přeskoč whitespace
+  while (std::isspace(sc.peek())) {
+    sc.advance();
+  }
+
+  auto o = match_operator_token(sc);
+  if (o.has_value()) {
+    return o;
+  }
+
+  auto d = match_digit_token(sc);
+  if (d.has_value()) {
+    return d;
+  }
+
+  auto k = match_keyword_or_name_token(sc);
+  if (k.has_value()) {
+    return k;
+  }
+
+  // jinak jsme museli narazit na konec
+  if (!sc.is_at_end())
+    sc.throw_error();
+
+  return std::nullopt;
+}
+
+std::vector<Token> lex(std::string source) {
+  Scanner sc = Scanner(source);
+  std::vector<Token> ts;
+
+  while (!sc.error_occured) {
+    auto t = lex_one(sc);
+    if (!t.has_value()) {
+      break;
+    }
+    ts.push_back(t.value());
+  }
+
+  return ts;
+}
+
+int main() {
+  std::string source = "for (i = 0; i < n; i = i + 1)\nvar \x04";
+  std::vector<Token> ts = lex(source);
+
+  for (auto t : ts) {
+    std::cout << token_type_to_str(t.type) << "(" << t.row << ", " << t.col
+              << ", " << t.value << ") ";
+  }
+  std::cout << '\n';
+}
